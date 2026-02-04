c7cf992fc7f619c4e04875a385ebe4244043acf9
DiegoMedina <diegomdnp@gmail.com>
2026-02-03 21:33:37 -0300

    Fix: Habilitar CORS en NestJS

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Agregamos esta línea mágica:
-  app.enableCors(); 
+  // 🔥 CONFIGURACIÓN CORS PERMISIVA 🔥
+  // Esto permite que Vercel (y cualquiera) pueda hablar con tu API
+  app.enableCors({
+    origin: true, // Permite cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
